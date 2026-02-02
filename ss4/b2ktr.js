4c15ab6ae08b598329ef9cbf33130ec078652eb6
hung20071612-hue <hung20071612@gmail.com>
2026-02-02 07:29:14 +0700

    Add files via upload

--- ss4/b2ktr.js
+++ ss4/b2ktr.js
@@ -1,12 +1,14 @@
 let result = 0;
 for(let i = 1; i <= 50; i++) {
     if(i % 3 === 0 && i % 5 !== 0){
-        console.log(`Fizz`);
+        console.log(`Số ${i} in ra: Fizz`);
         result += i;
     } else if (i % 3 !== 0 && i % 5 === 0) {
-        console.log(`Buzz`);
+        console.log(`Số ${i} in ra: Buzz`);
     } else if (i % 3 === 0 && i % 5 === 0 ) {
-        console.log(`FizzBuzz`);
+        console.log(`Số ${i} in ra: FizzBuzz`);
+    } else {
+        console.log(`Số ${i} không in`);
     }
 }
 console.log(`Tổng của tất cả các số đã in ra chữ "Fizz" là: ${result}`);
